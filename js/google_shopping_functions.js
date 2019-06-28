2764b752b8df27fd4c453a29cec03aa3e583adbf
Yi Xin Ang <angyixin@gmail.com>
2019-06-28 16:58:52 +0800

    exploring objects

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -4,14 +4,78 @@
  * input: accepts full item data
  * output: returns the length of the items array
  */
+
+// getItems(objectData)
+
+
 function getItemsCount(itemData) {
   return itemData.items.length;
 }
 
+
+// get items
+
+
+
+var getItems = function(objectData){
+  var i =0;
+  while (i < objectData.items.length){
+    console.log(objectData.items[i]);
+    i++;
+  }
+  console.log("done");
+}
+
+
+
+// get items by brand
+// var getItemsByBrand = function(items,brand){
+//   var itemsByBrand = [];
+//   var i =0;
+//   while (i < items.items.length){
+//     if (items.items[i].product.brand === brand ){
+//       itemsByBrand.push(items.items[i].product.title);
+//       // var itemsBy[i] = [];
+//       // console.log(itemBy[i]);
+//     }
+//     i++;
+//   }
+//   console.log(itemsByBrand);
+// }
+
+// get items by brand
+var getItemsByBrand = function(items,brand){
+  var itemsByBrand = [];
+  for (var i = 0; i < items.items.length; i++){
+    if (items.items[i].product.brand === brand ){
+      itemsByBrand.push(items.items[i].product.title);
+      console.log(items.items[i].product.title);
+      // var itemsBy[i] = [];
+      // console.log(itemBy[i]);
+    }
+  }
+  return itemsByBrand;
+}
+
+// var itemsByCanon = getItemsByBrand(products, "Canon");
+
+// get items by brand
+
+// get items by author
+// var getItemsByAuthor = function(items,author){
+//   var itemsByAuthor = [];
+//   for (var i = 0; i < items.items.length; i++){
+//     // console.log(items.items[i].product.author.name);
+//     if (author === items.items[i].product.author.name){
+//       itemsByAuthor.push(items.items[i].product.title);
+//       console.log([i] + " , " + (items.items[i].product.author.name) + " - " + itemsByAuthor );
+//       }
+//     }
+//   }
+
+
+// how can i create a new variable dynamically i.e. itemsInCanon
+
 /*
  * Define and use your functions here
  */
-
-// output item count using the getItemsCount function
-console.log('Item Count: ' + getItemsCount(data));
-
---
+++ script.js
@@ -0,0 +1,11 @@
+// output item count using the getItemsCount function
+// console.log('Item Count: ' + getItemsCount(products));
+
+// output item using the getItems function
+// console.log(getItems(products));
+
+// output item brands using the getItemsByBrand function
+// getItemsByBrand(products,"Nikon");
+
+// output item brands using the getItemsByAuthor function
+// console.log(getItemsByAuthor(products,"Target"));
